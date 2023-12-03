56d7f679f240b6c352854d0b72a109444ecbf019
Viktoriia <pecourses>
2023-12-03 17:24:07 +0200

    Add controllers base + errorHandler

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const { errorHandlers } = require('./middleware');
+const { authController } = require('./controllers');
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/signUp', authController.signUp);
+app.get('/login', authController.login);
+
+app.use(errorHandlers.errorHandler);
 
 module.exports = app;
---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+module.exports.authController = require('./authController'); // {login, signUp}
+
+// {
+//   authController: {login, signUp},
+//   contactsController: {createContact, getContacts, ...}
+// }
