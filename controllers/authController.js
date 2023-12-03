b741a7403d0f79a0a28fb9ecf39be7370f3b98e7
Viktoriia <pecourses>
2023-12-03 17:51:47 +0200

    Add user signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,30 @@
+const createHttpError = require('http-errors');
+
+const users = {
+  'user1@com': {
+    id: Math.random(),
+    name: 'Test',
+    email: 'user1@com',
+    password: '123456',
+  },
+};
+
 module.exports.login = (req, res, next) => {
   res.status(200).send('ok');
 };
 
-module.exports.signUp = (req, res, next) => {};
+module.exports.signUp = (req, res, next) => {
+  // отримати дані {name, email, password}
+  const { body } = req;
+  // перевірити, чи користувач з таким імейлом існує
+  if (body.email in users) {
+    // якщо існує, то помилка
+    return next(createHttpError(409, 'User with this email already exist'));
+  }
+  // інакше створити кори-ча
+  users[body.email] = { ...body, id: Math.random() };
+  // відправити {id, name, email}
+  const preparedUser = { ...users[body.email] };
+  delete preparedUser.password;
+  res.status(201).send(preparedUser);
+};
